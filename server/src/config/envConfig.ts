ad5484ab86308ddb8fe602f18224eeb5e6ed9076
seunghyeon <ksh990913@gmail.com>
2023-05-24 10:00:12 +0900

    Feat: envConfig.ts 파일에 환경 설정 내용 추가
    - serverProps: session 프로퍼티 추가
    - redisProps: redis 프로퍼티 추가
    - mysqlProps: mysql 프로퍼티 추가
    - sessionProps: session 프로퍼티 추가

---
+++ server/src/config/envConfig.ts
@@ -0,0 +1,32 @@
+export default {
+  serverProps: {
+    port: Number(process.env.SERVER_PORT) ?? 0,
+  },
+  redisProps: {
+    socket: {
+      host: process.env.REDIS_HOST ?? '',
+      port: Number(process.env.REDIS_PORT) ?? 0,
+      reconnectStrategy:
+        process.env.REDIS_RECONNECT_STRATEGY === 'false'
+          ? false
+          : Math.min(Number(process.env.REDIS_RECONNECT_STRATEGY) * 50, 1000),
+    },
+  },
+  mysqlProps: {
+    host: process.env.MYSQL_HOST ?? '',
+    port: Number(process.env.MYSQL_PORT) ?? 0,
+    user: process.env.MYSQL_USER ?? '',
+    password: process.env.MYSQL_PASSWORD ?? '',
+    database: process.env.MYSQL_DATABASE ?? '',
+  },
+  sessionProps: {
+    secret: process.env.COOKIE_SECRET ?? '',
+    cookieProps: {
+      httpOnly: process.env.COOKIE_HTTP_ONLY === 'true',
+      path: process.env.COOKIE_PATH ?? '',
+      maxAge: Number(process.env.COOKIE_EXP) ?? 0,
+      domain: process.env.COOKIE_DOMAIN ?? '',
+      secure: process.env.COOKIE_SECURE === 'true',
+    },
+  },
+} as const;
