8f22b6c3ddeb4ffdb2d944a4df2c12e15f949474
seunghyeon <ksh990913@gmail.com>
2023-05-26 15:47:21 +0900

    Style: tailwind.config.js 테마 색상과 그림자 설정 추가
    - 테마 색상 추가1: skyblue-100부터 skyblue-900까지
    - 테마 색상 추가2: black-025부터 black-900까지
    - 그림자 설정 추가: headerShadow

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,37 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        'skyblue-100': '#54E2DB',
+        'skyblue-200': '#52E0D9',
+        'skyblue-300': '#50DED7',
+        'skyblue-400': '#4EDCD5',
+        'skyblue-500': '#4CDAD3',
+        'skyblue-600': '#4AD8D1',
+        'skyblue-700': '#48D6CF',
+        'skyblue-800': '#46D4CD',
+        'skyblue-900': '#44D2CB',
+
+        'black-025': '#F8F9F9',
+        'black-050': '#F1F2F3',
+        'black-070': '#E3E5E8',
+        'black-100': '#D6D9DC',
+        'black-150': '#D8DBDE',
+        'black-200': '#BABFC4',
+        'black-350': '#9199A1',
+        'black-500': '#6A737C',
+        'black-600': '#525960',
+        'black-700': '#434343',
+        'black-750': '#3B4045',
+        'black-800': '#232629',
+        'black-900': '#121210',
+      },
+      boxShadow: {
+        headerShadow: '0px 2px 6px rgba(0, 0, 0, 0.04)',
+      },
+      screens: {
+        onlyMobile: { max: '767px' },
+      },
       minWidth: px0To1000,
       minHeight: px0To1000,
       maxWidth: px0To1000,
@@ -21,9 +52,6 @@ module.exports = {
       margin: px0To1000,
       borderRadius: px0To30,
       fontSize: px0To64,
-      screens: {
-        onlyMobile: { max: '767px' },
-      },
     },
   },
   '@tailwind base': {
