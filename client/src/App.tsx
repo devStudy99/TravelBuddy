47f70d6692c00a3d40320d0fc501c2e3129ef6b0
devStudy99 <ksh990913@gmail.com>
2023-05-26 17:30:03 +0900

    Design: Header 미디어 쿼리 적용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
+import '@assets/style/utilities.css';
+import { useMediaQuery } from 'react-responsive';
+import PcHeader from '@components/Header/PcHeader';
+import MHeader from '@components/Header/MHeader';
 
 function App() {
-  return <div className="App"></div>;
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const [dropDown, setDropDown] = useState(false);
+
+  return (
+    <div>{isMobile ? <MHeader dropDown={dropDown} setDropDown={setDropDown} /> : <PcHeader />}</div>
+  );
 }
 
 export default App;
