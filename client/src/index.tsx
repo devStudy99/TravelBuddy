bf85eb18019d0e9ab45045f0be5d4af7339a5956
devStudy99 <ksh990913@gmail.com>
2023-06-06 17:15:05 +0900

    Design: antd, swal, tailwind custom css 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '@app/store';
+import { ConfigProvider } from 'antd';
+import { theme } from '@styles/antd-theme.styles';
 import App from './App';
 import './App.css';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ConfigProvider theme={theme}>
+          <App />
+        </ConfigProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
---
+++ client/src/styles/antd-theme.styles.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  token: {
+    colorPrimary: '#4cdad3',
+  },
+};
