c5e2dcaf25d2ccfbff6c82bdc684fe3b6e0ca43d
devStudy99 <ksh990913@gmail.com>
2023-06-06 17:35:18 +0900

    Feat: Redux 스토어 구성 및 설정 추가

---
+++ client/src/app/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import authClient from '@api/authClient';
+
+const store = configureStore({
+  reducer: {
+    [authClient.reducerPath]: authClient.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authClient.middleware),
+  devTools: process.env.NODE_ENV !== 'production',
+});
+
+setupListeners(store.dispatch);
+
+export default store;
