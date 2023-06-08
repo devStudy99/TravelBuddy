c24dc5c249c151c230ff94c73a80438cecde56c2
devStudy99 <ksh990913@gmail.com>
2023-06-08 22:40:45 +0900

    Feat: 로그인 기능 구현 완료

---
+++ client/src/components/Auth/Login/LoginForm.styles.ts
@@ -0,0 +1,19 @@
+import tw from 'tailwind-styled-components';
+
+export const FindPasswordWrapper = tw.div`
+  flex
+  w-full
+  justify-end
+`;
+
+export const FindPasswordText = tw.p`
+  mb-24
+  mt-8
+  text-right
+  text-14
+  text-black-200
+`;
+
+export const LoginLine = tw.div`
+  login-line
+`;
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,3 @@
+import LoginContainer from '@components/Auth/Login/LoginContainer';
+
+export const Login = () => <LoginContainer />;
