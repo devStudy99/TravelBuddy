2cb382bd1369dc68d5166c3ce8b7c2492f64df1f
devStudy99 <ksh990913@gmail.com>
2023-06-09 03:04:00 +0900

    Feat: passport 미들웨어, error 미들웨어 추가

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,17 @@ import morgan from 'morgan';
 import cors from 'cors';
 import path from 'path';
 import session from 'express-session';
+import passport from 'passport';
 
-import sessionConfig from '@src/config/sessionConfig';
-import { testConnection } from '@src/config/mysqlConfig';
-import authRouter from '@src/routers/authRouter';
+import sessionConfig from '@config/sessionConfig';
+import { testConnection } from '@config/mysqlConfig';
+import passportConfig from '@passport/index';
+import authRouter from '@routers/authRouter';
+import userRouter from '@routers/userRouter';
+import ErrorHandler from '@src/middlewares/errorMiddleware';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+passportConfig();
 const app = express();
 
 if (process.env.NODE_ENV === 'production') {
@@ -34,8 +39,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session(sessionConfig));
+app.use(passport.initialize());
+app.use(passport.session());
 testConnection();
 
 app.use('/auth', authRouter);
+app.use('/users', userRouter);
+
+app.use(ErrorHandler);
 
 export default app;
