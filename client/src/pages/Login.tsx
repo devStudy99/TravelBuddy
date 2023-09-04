7edb96b2eb45a6cd7cf0b2101d84d235f5dc7d67
devStudy99 <ksh990913@gmail.com>
2023-09-04 22:16:44 +0900

    Remove: LoginContainer, SignupContainer 삭제

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,3 +1,30 @@
-import LoginContainer from '@components/Auth/Login/LoginContainer';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import LoginForm from '@Login/LoginForm';
+import {
+  AuthCard,
+  AuthWrapper,
+  AuthShortCutContainer,
+  AuthShortCutText,
+  AuthStatusText,
+  LoginWrapper,
+} from '@Auth/AuthContainer.styles';
 
-export const Login = () => <LoginContainer />;
+export const Login = (): JSX.Element => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  return (
+    <LoginWrapper $isMobile={isMobile}>
+      <AuthWrapper>
+        <AuthCard $isMobile={isMobile}>
+          <LoginForm />
+        </AuthCard>
+        <AuthShortCutContainer>
+          <AuthStatusText>아직 트래블버디 회원이 아니신가요?</AuthStatusText>
+          <Link to="/signup">
+            <AuthShortCutText>회원가입 바로가기</AuthShortCutText>
+          </Link>
+        </AuthShortCutContainer>
+      </AuthWrapper>
+    </LoginWrapper>
+  );
+};
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,3 +1,30 @@
-import SignupContainer from '@components/Auth/SignUp/SignUpContainer';
+import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import SignUpForm from '@SignUp/SignUpForm';
+import {
+  AuthCard,
+  AuthWrapper,
+  AuthShortCutContainer,
+  AuthShortCutText,
+  AuthStatusText,
+  SignupWrapper,
+} from '@Auth/AuthContainer.styles';
 
-export const Signup = () => <SignupContainer />;
+export const Signup = (): JSX.Element => {
+  const isMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  return (
+    <SignupWrapper $isMobile={isMobile}>
+      <AuthWrapper>
+        <AuthCard $isMobile={isMobile}>
+          <SignUpForm />
+        </AuthCard>
+        <AuthShortCutContainer>
+          <AuthStatusText>이미 계정이 있으신가요?</AuthStatusText>
+          <Link to="/login">
+            <AuthShortCutText>로그인 바로가기</AuthShortCutText>
+          </Link>
+        </AuthShortCutContainer>
+      </AuthWrapper>
+    </SignupWrapper>
+  );
+};
