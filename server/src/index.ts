fe3c608c27bfcfb293fddf7f2673bfd9f43a8ef9
devStudy99 <ksh990913@gmail.com>
2023-11-28 13:25:43 +0900

    Feat: socket 설정 추가

--- server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import server from '@src/server';
-import logger from '@utils/loggerUtils';
-
-server.listen(process.env.SERVER_PORT || 3001, () => {
-  logger.info('Server is running on port 8080');
-});
--- server/src/server.ts
+++ server/src/server.ts
@@ -8,16 +8,35 @@ import path from 'path';
 import session from 'express-session';
 import passport from 'passport';
 
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import configureSocket from './utils/io';
+
+import logger from '@utils/loggerUtils';
+
 import sessionConfig from '@config/sessionConfig';
 import { testConnection } from '@config/mysqlConfig';
 import passportConfig from '@passport/index';
 import authRouter from '@routers/authRouter';
 import userRouter from '@routers/userRouter';
+import accompanyRouter from '@routers/accompanyRouter';
 import ErrorHandler from '@src/middlewares/errorMiddleware';
+import chatRouter from './routers/chatRouter';
+import mypageRouter from './routers/mypageRouter';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 passportConfig();
 const app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  },
+});
+
+configureSocket(io);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(hpp());
@@ -37,7 +56,10 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use(session(sessionConfig));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -45,7 +67,12 @@ testConnection();
 
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
+app.use('/accompany', accompanyRouter);
+app.use('/chat', chatRouter);
+app.use('/mypage', mypageRouter);
 
 app.use(ErrorHandler);
 
-export default app;
+httpServer.listen(process.env.SERVER_PORT || 8080, () => {
+  logger.info('Server is running on port 8080');
+});
